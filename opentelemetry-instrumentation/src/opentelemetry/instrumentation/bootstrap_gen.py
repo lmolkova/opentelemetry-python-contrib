8f6d2320bd92e931f0d5fd3e077866575dbf3bae
Liudmila Molkova <limolkova@microsoft.com>
2024-09-13 15:25:37 -0700

    Per package release

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -121,6 +121,10 @@ libraries = [
         "instrumentation": "opentelemetry-instrumentation-mysqlclient==0.49b0.dev",
     },
     {
+        "library": "openai >= 0.27.0",
+        "instrumentation": "opentelemetry-instrumentation-openai==1.1.0.dev",
+    },
+    {
         "library": "pika >= 0.12.0",
         "instrumentation": "opentelemetry-instrumentation-pika==0.49b0.dev",
     },
